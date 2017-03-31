63e4e25d833a9df81a7b5a8eeda89c87f966cfdd
wangyong <874999821@qq.com>
2017-03-31 11:48:15 +0800

    加入选择相册时候+的显示  并加入图片的缩放功能

--- XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/photo/PhotoActivity.java
+++ XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/photo/PhotoActivity.java
@@ -3,16 +3,22 @@ package com.example.administrator.xrefreshviewdemo.photo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.administrator.xrefreshviewdemo.R;
+import com.jph.takephoto.app.TakePhoto;
 import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.compress.CompressConfig;
 import com.jph.takephoto.model.TImage;
 import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.model.TakePhotoOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoActivity extends TakePhotoActivity implements View.OnClickListener {
 
@@ -20,16 +26,103 @@ public class PhotoActivity extends TakePhotoActivity implements View.OnClickList
     private Button btnPickBySelect;
     private Button btnPickByTake;
 
+    private RecyclerView recyclerView;
+    private PhotoAdapter photoAdapter;
+    Context mcontext;
+    private List<TImage> selectMedia = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_photo);
         View contentView = LayoutInflater.from(this).inflate(R.layout.common_layout, null);
         customHelper = CustomHelper.of(contentView);
+        mcontext = this;
         initView();
+
+        initRecylerView();
+
 
     }
 
+    private void initRecylerView() {
+        //设置RecyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        photoAdapter = new PhotoAdapter(this, onAddPicListener, onPicClickListener);
+        photoAdapter.setSelectMax(8);
+        recyclerView.setAdapter(photoAdapter);
+    }
+
+    //加号的点击事件
+    private PhotoAdapter.onAddPicListener onAddPicListener = new PhotoAdapter.onAddPicListener() {
+        @Override
+        public void onAddPicClick(int type, int position) {
+            switch (type) {
+                case 0:
+//                    new AlertView("上传图片", null, "取消", null,
+//                            new String[]{"拍照", "从相册中选择"},
+//                            PhotoActivity.this, AlertView.Style.ActionSheet, new OnItemClickListener() {
+//                        @Override
+//                        public void onItemClick(Object o, int position) {
+//                            TakePhoto takePhoto = getTakePhoto();
+//                            //获取TakePhoto图片路径
+//                            File file = new File(Environment.getExternalStorageDirectory(), "/photo/" + System.currentTimeMillis() + ".jpg");
+//                            if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
+//                            Uri imageUri = Uri.fromFile(file);
+//
+//                            configCompress(takePhoto);
+//                            configTakePhotoOption(takePhoto);
+//                            switch (position) {
+//                                case 0:
+//                                    takePhoto.onPickFromCapture(imageUri);
+//                                    break;
+//                                case 1:
+//                                    //设置最多几张
+//                                    takePhoto.onPickMultiple(3);
+//                                    break;
+//                            }
+//                        }
+//                    }).show();
+                    break;
+                case 1:
+                    // 删除图片
+                    selectMedia.remove(position);
+                    photoAdapter.notifyItemRemoved(position);
+                    break;
+            }
+        }
+    };
+
+    //图片点击事件
+    private PhotoAdapter.onPicClickListener onPicClickListener = new PhotoAdapter.onPicClickListener() {
+        @Override
+        public void onPicClick(View view, int position) {
+            String url = selectMedia.get(position).getOriginalPath();
+            ResultActivity.startActivity(mcontext, url);
+        }
+    };
+
+    //设置Takephoto 使用TakePhoto自带的相册   照片旋转角度纠正
+    private void configTakePhotoOption(TakePhoto takePhoto) {
+        TakePhotoOptions.Builder builder = new TakePhotoOptions.Builder();
+        builder.setWithOwnGallery(true);
+        builder.setCorrectImage(true);
+        takePhoto.setTakePhotoOptions(builder.create());
+    }
+
+    //设置takephoto的照片使用压缩
+    private void configCompress(TakePhoto takePhoto) {
+        CompressConfig config;
+        config = new CompressConfig.Builder()
+                .setMaxSize(102400)
+                .setMaxPixel(800)
+                .enableReserveRaw(true)
+                .create();
+        takePhoto.onEnableCompress(config, false);
+    }
+
+    //===========================================================
 
     @Override
     public void takeCancel() {
@@ -44,13 +137,24 @@ public class PhotoActivity extends TakePhotoActivity implements View.OnClickList
     @Override
     public void takeSuccess(TResult result) {
         super.takeSuccess(result);
+
         showImg(result.getImages());
     }
 
     private void showImg(ArrayList<TImage> images) {
-        Intent intent = new Intent(this, ResultActivity.class);
-        intent.putExtra("images", images);
-        startActivity(intent);
+//        Intent intent = new Intent(this, ResultActivity.class);
+//        intent.putExtra("images", images);
+//        startActivity(intent);
+
+        for (int i = 0; i < images.size(); i++) {
+            if (images.get(i).getCompressPath() != null) {
+                selectMedia.add(images.get(i));
+            }
+        }
+        if (selectMedia != null) {
+            photoAdapter.setList(selectMedia);
+            photoAdapter.notifyDataSetChanged();
+        }
     }
 
     public static void startActivity(Context context) {
--- XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/photo/ResultActivity.java
+++ XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/photo/ResultActivity.java
@@ -1,65 +1,40 @@
 package com.example.administrator.xrefreshviewdemo.photo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
+import com.bm.library.PhotoView;
 import com.bumptech.glide.Glide;
 import com.example.administrator.xrefreshviewdemo.R;
-import com.jph.takephoto.model.TImage;
 
-import java.io.File;
-import java.util.ArrayList;
+import static com.example.administrator.xrefreshviewdemo.R.id.img;
 
 
-/**
- * - 支持通过相机拍照获取图片
- * - 支持从相册选择图片
- * - 支持从文件选择图片
- * - 支持多图选择
- * - 支持批量图片裁切
- * - 支持批量图片压缩
- * - 支持对图片进行压缩
- * - 支持对图片进行裁剪
- * - 支持对裁剪及压缩参数自定义
- * - 提供自带裁剪工具(可选)
- * - 支持智能选取及裁剪异常处理
- * - 支持因拍照Activity被回收后的自动恢复
- * Author: crazycodeboy
- * Date: 2016/9/21 0007 20:10
- * Version:4.0.0
- * 技术博文：http://www.cboy.me
- * GitHub:https://github.com/crazycodeboy
- * Eamil:crazycodeboy@gmail.com
- */
 public class ResultActivity extends Activity {
-    ArrayList<TImage> images;
+
+    private PhotoView mPhotoView;
+    private static final String KEY_IMG = "IMG";
+    public String img_value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_layout);
-        images= (ArrayList<TImage>) getIntent().getSerializableExtra("images");
-        showImg();
+        img_value = "file://" + getIntent().getStringExtra(KEY_IMG);
+        initView();
     }
-    private void showImg() {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.llImages);
-        for (int i = 0, j = images.size(); i < j - 1; i += 2) {
-            View view = LayoutInflater.from(this).inflate(R.layout.image_show, null);
-            ImageView imageView1 = (ImageView) view.findViewById(R.id.imgShow1);
-            ImageView imageView2 = (ImageView) view.findViewById(R.id.imgShow2);
-            Glide.with(this).load(new File(images.get(i).getCompressPath())).into(imageView1);
-            Glide.with(this).load(new File(images.get(i + 1).getCompressPath())).into(imageView2);
-            linearLayout.addView(view);
-        }
-        if (images.size() % 2 == 1) {
-            View view = LayoutInflater.from(this).inflate(R.layout.image_show, null);
-            ImageView imageView1 = (ImageView) view.findViewById(R.id.imgShow1);
-            Glide.with(this).load(new File(images.get(images.size() - 1).getCompressPath())).into(imageView1);
-            linearLayout.addView(view);
-        }
 
+    public static void startActivity(Context context, String url) {
+        Intent starter = new Intent(context, ResultActivity.class);
+        starter.putExtra(KEY_IMG, url);
+        context.startActivity(starter);
+    }
+
+    private void initView() {
+        mPhotoView = (PhotoView) findViewById(img);
+        mPhotoView.enable();
+        Glide.with(this).load(img_value).into(mPhotoView);
     }
 }
