f9adfb19bb386de89e7226d8676779fe22989448
wangyong <874999821@qq.com>
2017-04-14 11:49:13 +0800

    增加单个滑轮的滚动

--- XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/wheelview/WheelActivity.java
+++ XRefreshViewDemo2/app/src/main/java/com/example/administrator/xrefreshviewdemo/wheelview/WheelActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import java.util.Calendar;
 
 import cn.qqtheme.framework.picker.DatePicker;
 import cn.qqtheme.framework.picker.TimePicker;
+import cn.qqtheme.framework.widget.WheelView;
 
 public class WheelActivity extends Activity {
 
@@ -32,7 +34,27 @@ public class WheelActivity extends Activity {
         setContentView(R.layout.activity_wheel);
         initTime();
         initDate();
+        initSingleWheel();
 
+    }
+
+    public static final int MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT;
+    public static final int WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT;
+    protected int textColorNormal = WheelView.TEXT_COLOR_NORMAL;
+    protected int textColorFocus = WheelView.TEXT_COLOR_FOCUS;
+    protected int offset = WheelView.ITEM_OFF_SET;
+    protected boolean cycleDisable = true;
+    protected WheelView.LineConfig lineConfig;
+
+    private void initSingleWheel() {
+        WheelView wheelView = (WheelView)findViewById(R.id.wheelview_single);
+        wheelView.setItems(new String[]{"少数民族", "贵州穿青人", "不在56个少数民族之列", "第57个民族"}, 1);
+        wheelView.setOnWheelListener(new WheelView.OnWheelListener() {
+            @Override
+            public void onSelected(boolean isUserScroll, int index, String item) {
+//                textView.setText("index=" + index + ",item=" + item);
+            }
+        });
     }
 
 
@@ -42,6 +64,7 @@ public class WheelActivity extends Activity {
         TimePicker picker = new TimePicker(this, TimePicker.HOUR_24);
         picker.setRangeStart(0, 0);//00:00
         picker.setRangeEnd(23, 59);//23:59
+//        picker.setCycleDisable(false);//禁用循环
         int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
         picker.setSelectedItem(currentHour, currentMinute);
